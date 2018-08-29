72322cbe1d8d02465e601c6e529019e40b639b2c
zhangxiuke <812682425@qq.com>
2018-08-29 18:07:18 +0800

    测试冲突git解决

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -17,16 +17,16 @@ public class DemoApplicationTests {
 	public void contextLoads() {
 		TreeSet<StudentVo> stu = new TreeSet<StudentVo>(new MyComparator());
 
-		StudentVo s1 = new StudentVo("zhangsan",18);
+		StudentVo s6 = new StudentVo("zhangsan",18);
 		StudentVo s2 = new StudentVo("lisi",20);
 		StudentVo s3 = new StudentVo("wangwu",17);
 		StudentVo s4 = new StudentVo("maliu",23);
 		StudentVo s5 = new StudentVo("heiqi",16);
-		stu.add(s1);
 		stu.add(s2);
 		stu.add(s3);
 		stu.add(s4);
 		stu.add(s5);
+		stu.add(s6);
 
 		for (StudentVo s : stu){
 			System.out.println(s.getName()+"=============="+s.getAge());
